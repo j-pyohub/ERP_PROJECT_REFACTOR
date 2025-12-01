2b95a500faa2b204875fc0cebd4325b8de4eb808
j-pyohub <pyosuejung@gmail.com>
2025-12-01 18:45:16 +0900

    wip: addSalesOrder

--- src/main/java/com/erp/controller/SalesOrderController.java
+++ src/main/java/com/erp/controller/SalesOrderController.java
@@ -3,12 +3,19 @@ package com.erp.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("sales")
 public class SalesOrderController {
-    @GetMapping("salesOrder")
+    @GetMapping("/salesOrder")
     public String salesOrder() {
         return "sales/salesOrderManagerUI";
+    }
+
+    @GetMapping("/addSalesOrder")
+    public String addSalesOrder() {
+        return "sales/salesOrderAddUI";
     }
 }
