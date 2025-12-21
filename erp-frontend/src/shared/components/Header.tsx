1e3d35d9b8c0a96b99ecbfe5476b353dfe38e290
pyosj <pyosuejung@gmail.com>
2025-12-21 16:48:57 +0900

    fix: fix header dropdown menu

--- erp-frontend/src/shared/components/Header.tsx
+++ erp-frontend/src/shared/components/Header.tsx
@@ -1,3 +1,4 @@
+import Dropdown from "./Dropdown";
 import "./Header.css";
 
 function Header() {
@@ -7,50 +8,13 @@ function Header() {
           <button className="logo-btn">로고</button>
           <button className="company-name-btn">회사명</button>
         </div>
-        <div className="menu-item dropdown">
-          <span>매출 ▾</span>
-          <div className="dropdown-menu">
-            <a>매출 조회</a>
-            <a>매출 조회</a>
-            <a>주문 조회</a>
-          </div>
-        </div>
-        <div className="menu-item dropdown">
-          <span>품목/재고 ▾</span>
-          <div className="dropdown-menu">
-            <a>품목 조회</a>
-            <a>재고 현황 조회</a>
-            <a>재고 현황 조회</a>
-            <a>재고 변동 조회</a>
-            <a>재고 변동 조회</a>
-            <a>발주</a>
-            <a>발주 내역 조회</a>
-            <a>발주 내역 조회</a>
-            <a>발주 제안</a>
-          </div>
-        </div>
-
-        <div className="menu-item dropdown">
-          <span>메뉴 ▾</span>
-          <div className="dropdown-menu">
-            <a>메뉴 등록</a>
-            <a>메뉴 조회</a>
-            <a>판매 메뉴 관리</a>
-            <a>판매 메뉴 관리</a>
-          </div>
-        </div>
-
+        <Dropdown title="매출" items={["매출 조회", "주문 조회"]} />
+        <Dropdown title="품목/재고" items={["품목 조회", "재고 현황 조회", "재고 변동 조회", "발주", "발주 내역 조회", "발주 제안"]} />
+        <Dropdown title="메뉴" items={["메뉴 등록", "메뉴 조회", "판매 메뉴 관리"]} />
         <div className="menu-item">
           <span>직영점 목록</span>
         </div>
-
-        <div>
-          <span>계정관리 ▾</span>
-          <div className="dropdown-menu">
-            <a>계정 등록</a>
-            <a>계정 목록 조회</a>
-          </div>
-        </div>
+        <Dropdown title="계정 관리" items={["계정 등록", "계정 목록 조회"]} />
       <div className="right-section">
         <span>사용자명</span>
         <button className="logout-btn">로그아웃</button>
