2a478ad4e8a3f34231dbda0fc2e7fde93c5d7429
j-pyohub <pyosuejung@gmail.com>
2025-12-23 17:29:51 +0900

    style: rename btn (custom-btn -> yellow-btn)

--- erp-frontend/src/features/menu/pages/MenuListPage.tsx
+++ erp-frontend/src/features/menu/pages/MenuListPage.tsx
@@ -18,8 +18,8 @@ const [releaseStatus, setReleaseStatus] = useState("");
                 <h2 className="font-bold mr-4">메뉴 조회</h2>
             </div>
                 <div>
-                    <Button className="me-2 custom-btn" onClick = {goToMenuAdd}>메뉴 등록</Button>
-                    <Button className="me-2 custom-btn" onClick={goToStoreMenu}>판매 메뉴 보기</Button>
+                    <Button className="me-2 yellow-btn" onClick = {goToMenuAdd}>메뉴 등록</Button>
+                    <Button className="me-2 yellow-btn" onClick={goToStoreMenu}>판매 메뉴 보기</Button>
                 </div>
             </div>
         <MenuFilterBar  menuCategory={menuCategory}
