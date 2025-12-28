e8bd3f1e26e5c4551b1201b923e6ffc32f8f82f9
pyosj <pyosuejung@gmail.com>
2025-12-28 14:17:38 +0900

    wip: stock page

--- erp-frontend/src/features/stock/pages/StoreStockPage.tsx
+++ erp-frontend/src/features/stock/pages/StoreStockPage.tsx
@@ -1,11 +1,24 @@
+import Button from "../../../shared/components/Button";
+import { LabeledInput } from "../../../shared/components/LabeledInput";
 import StockFilterBar from "../components/StockFilterBar";
 
 export default function StoreStockPage() {
     return (
         <>
-            <div className="flex justify-between items-center mb-8">
+            <div className="flex items-end mb-8">
                 <h2 className="font-bold mr-4">재고 조회</h2>
-                ㅏ
+
+            </div>
+            <div className="flex">
+                <LabeledInput 
+                    label="직영점 검색"
+                    wrapperClassName="flex justify-end gap-2"
+                    labelClassName="whitespace-nowrap"
+                    inputClassName="border rounded h-10 px-4 w-[200px]"
+                />
+                <Button className="yellow-btn h-10 whitespace-nowrap">
+                    검색
+                </Button>
             </div>
 
             <StockFilterBar />
